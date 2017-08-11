b7f4e7c9bb045f9f6afeac06c450295d798de48c
Gabriel Souza <gabrie.ssouza@gmail.com>
2017-08-11 19:57:41 -0300

    first Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+
+const value = '';
+
+app.get('/on', (req, res)=>{
+  value = 'on';
+  res.send('Ligando...');
+})
+
+app.get('off', (req, res)=>{
+  value = 'off';
+  res.send('Desligando...')
+})
+
+app.get('/', (req, res)=>{
+  res.send(value);
+})
